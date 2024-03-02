151815e8a6e6f3151c80a4845e9d1e1a7653efef
Edwin Jude <edwin.judefrancis@gmail.com>
2024-03-02 22:46:58 +0530

    category CRUD

--- controllers/category.js
+++ controllers/category.js
@@ -2,9 +2,6 @@ const Category = require('../models/category');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
 
-
-
-
 exports.categoryById = (req, res, next, id) => {
     Category.findById(id)
         .then(category => {
@@ -42,4 +39,58 @@ exports.create = (req, res) => {
 
 exports.read=(req,res)=>{
     return res.json(req.category)
+}
+
+
+
+exports.update=(req,res)=>{
+
+    const category=req.category
+    category.name=req.body.name
+    category.save((err,data)=>{
+        if(err){
+            return res.json(400).json({
+                error:errorHandler(err)
+            })
+
+        }
+
+        res.json(data)
+    })
+
+}
+
+
+
+exports.remove=(req,res)=>{
+    
+    const category=req.category
+    
+    category.remove((err,data)=>{
+        if(err){
+            return res.json(400).json({
+                error:errorHandler(err)
+            })
+
+        }
+
+        res.json({
+            message:'Category deleted';
+        })
+    })
+}
+
+
+
+exports.list=(req,res)=>{
+    
+    Category.find().exec((err,data)=>{
+        if(err){
+            return res.status(400).json({
+                error:errorHandler(err)
+            })
+        }
+
+        res.json(data);
+    })
 }
