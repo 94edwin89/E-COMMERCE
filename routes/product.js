0c093d3e7b3424a3404076458843eb755370e060
Edwin Jude <edwin.judefrancis@gmail.com>
2024-03-06 09:51:34 +0530

    small documentation in API

--- routes/product.js
+++ routes/product.js
@@ -12,11 +12,18 @@ router.post('/product/create/:userId',requireSignin,isAdmin,isAuth,create) ;
 router.delete('/product/:productId/:userId',requireSignin,isAdmin,isAuth,remove);
 router.put('/product/:productId/:userId',requireSignin,isAdmin,isAuth,update);
 
-
+// route use to display all the product lists
 router.get('/products',list);
+
+// route use to display most sold or releted products
 router.get('/products/related/:productId',listRelated);
+
+// route for category list in products
 router.get('/products/categories',listCategories); 
+
 router.post('/products/by/search',listBySearch);
+
+// route for photo upload
 router.get('/product/photo/:productId',photo)
 
 router.param('userId',userById);
--- routes/user.js
+++ routes/user.js
@@ -4,13 +4,21 @@ const router=express.Router()
 const{requireSignin,isAuth, isAdmin}=require('../controllers/auth')
 const{userById,read,update}=require('../controllers/user')
 
+
+
+// route use to sign in by admin
+
 router.get('/secret/:userId',requireSignin,isAuth,isAdmin,(req,res)=>{
     res.json({
         user:req.profile
     })
 });
 
+// route for read users from db hit
+
 router.get('/user/:userId',requireSignin,isAuth,read)
+
+//  route for update user details
 
 router.put('/user/:userId',requireSignin,isAuth,update)
 
