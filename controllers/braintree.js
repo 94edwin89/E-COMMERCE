5811f4e29f4f0a84d08a72bcfd40c72455466e9f
Edwin Jude <edwin.judefrancis@gmail.com>
2024-03-30 00:10:36 +0530

    braintree setup in Backend

--- controllers/braintree.js
+++ controllers/braintree.js
@@ -2,7 +2,14 @@ const User = require("../models/user");
 const braintree = require("braintree");
 require("dotenv").config();
 
-const gateway = braintree.connect({
+
+// const gateway = braintree.connect({
+//     environment: braintree.Environment.Sandbox,
+//     merchantId: process.env.BRAINTREE_MERCHANT_ID,
+//     publicKey: process.env.BRAINTREE_PUBLIC_KEY,
+//     privateKey: process.env.BRAINTREE_PRIVATE_KEY
+// })
+const gateway = new braintree.BraintreeGateway({
     environment: braintree.Environment.Sandbox,
     merchantId: process.env.BRAINTREE_MERCHANT_ID,
     publicKey: process.env.BRAINTREE_PUBLIC_KEY,
